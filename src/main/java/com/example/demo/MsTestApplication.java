61fba483bc8758aedbfaa74c7649de06f957f554
tomdeconinck2 <tom.de.coninck@devoteam.com>
2020-09-29 09:01:40 +0200

    first

---
+++ src/main/java/com/example/demo/MsTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsTestApplication.class, args);
+	}
+
+}
